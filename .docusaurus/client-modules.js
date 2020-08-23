b00816d3967b028b1efeed3812a239158a518979
Endrit <endrit_demaj@hotmail.com>
2020-08-23 19:27:05 +0200

    Deploy website - based on 12b734b899dd3d2f41b00ad7012ef33d21952644

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/home/endrit/Documents/endritdemaj.github.io/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/endrit/Documents/endritdemaj.github.io/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/endrit/Documents/endritdemaj.github.io/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/endrit/Documents/endritdemaj.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/home/endrit/Documents/endritdemaj.github.io/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-  require("/home/endrit/Documents/endritdemaj.github.io/src/css/custom.css"),
-];
