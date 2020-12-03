29a373677c4a971a592494cf0baa2f2b443839af
endritdemaj <endrit_demaj@hotmail.com>
2020-12-03 10:53:03 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    //Docusaurus: ['doc1', 'doc2', 'doc3'],
+    Docusaurus: ['doc1', 'doc2', 'doc3'],
     Documentation:['documentation/linuxcheatsheet','documentation/dockercheatsheet', 'documentation/kubernetescheatsheet',
                     'documentation/tivolicheatsheet', 'documentation/systemdcheatsheet','documentation/ansiblecheatsheet'],
     Features: ['mdx']
