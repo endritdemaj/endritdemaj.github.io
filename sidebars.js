63671d001f6808458a0f7d4569e1be3fd78fd8a8
b1traxx <endrit_demaj@hotmail.com>
2021-02-18 13:40:30 +0100

    'la'

--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
   someSidebar: {
     Docusaurus: ['doc1', 'doc2', 'doc3'],
     Documentation:['documentation/linuxcheatsheet','documentation/windowscheatsheet','documentation/dockercheatsheet', 'documentation/kubernetescheatsheet',
-                    'documentation/tivolicheatsheet', 'documentation/systemdcheatsheet','documentation/ansiblecheatsheet'],
+                    'documentation/tivolicheatsheet', 'documentation/systemdcheatsheet','documentation/ansiblecheatsheet', 'documentation/ibmdatastage'],
     Features: ['mdx']
   },
 };
